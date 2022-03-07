0cf42299a779297629d98f9ba45dc99fd3295b4e
Leonardo Begnossi <89605267+MadOrchid@users.noreply.github.com>
2022-03-07 00:08:51 -0300

    importando-img

--- cfb-teste-01/src/App.js
+++ cfb-teste-01/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+  const envia = "segundo333";
+  const codigo = () => {
+    return ('primero001')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{codigo()} Texto supremo  {codigo()}</h1>
+      <p>{envia}{codigo()} Texto</p>
+      <p>Texto2</p>
     </div>
-  );
+  )
 }
 
-export default App;
